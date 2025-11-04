1687bab6a1de3c2c33b9cbd7ce79e78210a7699b
Akseli-R <akselism@gmail.com>
2025-11-04 15:09:07 +0200

    hitlightning assets

--- TempoTabbies/Assets/Scripts/HitReg/HitManager.cs
+++ TempoTabbies/Assets/Scripts/HitReg/HitManager.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HitManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("GameObjects assigned to buttons")]
+    public GameObject leftTriggerObject;
+    public GameObject rightTriggerObject;
+    public GameObject leftBumperObject;
+    public GameObject rightBumperObject;
+    public GameObject leftStickObject;
+    public GameObject rightStickObject;
 
-    // Update is called once per frame
+    private Gamepad gamepad;
+
     void Update()
     {
-        
+        gamepad = Gamepad.current;
+        if (gamepad == null)
+            return;
+
+        // LT
+        if (leftTriggerObject != null)
+            leftTriggerObject.SetActive(gamepad.leftTrigger.isPressed);
+
+        // RT
+        if (rightTriggerObject != null)
+            rightTriggerObject.SetActive(gamepad.rightTrigger.isPressed);
+
+        // LB
+        if (leftBumperObject != null)
+            leftBumperObject.SetActive(gamepad.leftShoulder.isPressed);
+
+        // RB
+        if (rightBumperObject != null)
+            rightBumperObject.SetActive(gamepad.rightShoulder.isPressed);
+
+        // Left Stick Click
+        if (leftStickObject != null)
+            leftStickObject.SetActive(gamepad.leftStickButton.isPressed);
+
+        // Right Stick Click
+        if (rightStickObject != null)
+            rightStickObject.SetActive(gamepad.rightStickButton.isPressed);
     }
 }

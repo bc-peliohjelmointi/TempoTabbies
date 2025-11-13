0506e706ed86f26ab1bb4221e15aded21703db5f
Moona-T <s2300927@edu.bc.fi>
2025-11-13 12:12:55 +0200

    Kortti logiikka toimii
    Osaa antaa oikealle pelaajalle kortin ja pitäisi osata vaihtaa sceneä tarvittaessa.

--- TempoTabbies/Assets/Scripts/Cards2/CardManagerScript.cs
+++ TempoTabbies/Assets/Scripts/Cards2/CardManagerScript.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 using static CardDataScript;
 
 public class CardManagerScript : MonoBehaviour
@@ -12,16 +12,18 @@ public class CardManagerScript : MonoBehaviour
     [Header("Player Card Choices")]
     public PlayerScript Score;
 
+    [Header("UI asiat")]
+    public TextMeshProUGUI ValittuPelaaja;
 
-    public CardDataScript.CardData PlayerAChoice;
-    public CardDataScript.CardData PlayerBChoice;
+    /* public CardDataScript.CardData PlayerAChoice;
+     public CardDataScript.CardData PlayerBChoice;
 
-    private Gamepad padA;
-    private Gamepad padB;
-    private int indexA = 0;
-    private int indexB = 0;
-    private bool lockedA = false;
-    private bool lockedB = false;
+     private Gamepad padA;
+     private Gamepad padB;
+     private int indexA = 0;
+     private int indexB = 0;
+     private bool lockedA = false;
+     private bool lockedB = false;*/
 
     List<CardDataScript.CardData> KorttiLista; //Lista arvotuista korteista
 
@@ -30,8 +32,8 @@ public class CardManagerScript : MonoBehaviour
     {
         KorttiLista = new List<CardDataScript.CardData>();
 
-        if (Gamepad.all.Count > 0) padA = Gamepad.all[0];
-        if (Gamepad.all.Count > 1) padB = Gamepad.all[1];
+        /*if (Gamepad.all.Count > 0) padA = Gamepad.all[0];
+        if (Gamepad.all.Count > 1) padB = Gamepad.all[1];*/
 
         RandomizeCard();
     }
@@ -45,57 +47,59 @@ public class CardManagerScript : MonoBehaviour
             if (NoCardPlayer != null) //jos on pelaaja ilman korttia 
             {
                 //vaihtaa UI tektin pelaajaan vuoroon
+                ValittuPelaaja.text = NoCardPlayer._playerIndex + 1 + " valitse kortti";
                 //odotta
 
             }
             else //voi menn‰ eteenp‰in
             {
                 // change scene !!!!
+                SceneManager.LoadScene("Korttiscene");
             }
         }
         else
         {
-           //ei tule mit‰‰n, koska ei ole pelaajia ja t‰m‰ menee vain ohi
+            //ei tule mit‰‰n, koska ei ole pelaajia ja t‰m‰ menee vain ohi
         }
 
 
         if (AllCards == null || AllCards.Count == 0) return;
 
         // --- Player A selection ---
-        if (padA != null && !lockedA)
-        {
-            if (padA.dpad.left.wasPressedThisFrame) indexA = Mathf.Max(0, indexA - 1);
-            if (padA.dpad.right.wasPressedThisFrame) indexA = Mathf.Min(AllCards.Count - 1, indexA + 1);
+        /* if (padA != null && !lockedA)
+         {
+             if (padA.dpad.left.wasPressedThisFrame) indexA = Mathf.Max(0, indexA - 1);
+             if (padA.dpad.right.wasPressedThisFrame) indexA = Mathf.Min(AllCards.Count - 1, indexA + 1);
 
-            if (padA.buttonSouth.wasPressedThisFrame) // A button
-            {
-                PlayerAChoice = AllCards[indexA];
-                lockedA = true;
-                Debug.Log($"[CardSelect] Player A chose {PlayerAChoice}+ valinta(puuttuu)");
-            }
-        }
+             if (padA.buttonSouth.wasPressedThisFrame) // A button
+             {
+                 PlayerAChoice = AllCards[indexA];
+                 lockedA = true;
+                 Debug.Log($"[CardSelect] Player A chose {PlayerAChoice}+ valinta(puuttuu)");
+             }
+         }
 
-        // --- Player B selection ---
-        if (padB != null && !lockedB)
-        {
-            if (padB.dpad.left.wasPressedThisFrame) indexB = Mathf.Max(0, indexB - 1);
-            if (padB.dpad.right.wasPressedThisFrame) indexB = Mathf.Min(AllCards.Count - 1, indexB + 1);
+         // --- Player B selection ---
+         if (padB != null && !lockedB)
+         {
+             if (padB.dpad.left.wasPressedThisFrame) indexB = Mathf.Max(0, indexB - 1);
+             if (padB.dpad.right.wasPressedThisFrame) indexB = Mathf.Min(AllCards.Count - 1, indexB + 1);
 
-            if (padB.buttonSouth.wasPressedThisFrame)
-            {
-                PlayerBChoice = AllCards[indexB];
-                lockedB = true;
+             if (padB.buttonSouth.wasPressedThisFrame)
+             {
+                 PlayerBChoice = AllCards[indexB];
+                 lockedB = true;
 
-                Debug.Log($"[CardSelect] Player B chose {PlayerBChoice}");
-            }
-        }
+                 Debug.Log($"[CardSelect] Player B chose {PlayerBChoice}");
+             }
+         }
 
-        // Kun molemmat ovat valinneet kortin, peli voi alkaa
-        if (lockedA && lockedB)
-        {
-            Debug.Log("? Both players have chosen cards. Starting match...");
-            enabled = false;
-        }
+         // Kun molemmat ovat valinneet kortin, peli voi alkaa
+         if (lockedA && lockedB)
+         {
+             Debug.Log("? Both players have chosen cards. Starting match...");
+             enabled = false;
+         }*/
     }
     public void RandomizeCard()
     {
@@ -116,7 +120,7 @@ public class CardManagerScript : MonoBehaviour
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
         if (players.Length == 1)
         {
-            return null;
+            return Chosen;
         }
         else
         {
@@ -201,6 +205,18 @@ public class CardManagerScript : MonoBehaviour
     public void GiveCardToPlayer(CardData card)
     {
         PlayerScript script = findPlayerActive();
-        script.AllCards.Add(card);
+        if (script != null)
+        {
+            PlayerScript NoCard = findPlayerNoCard(script);
+            if (NoCard != null)
+            {
+                NoCard.AllCards.Add(card);
+            }
+
+        }
+        else
+        {
+            Debug.Log("Korttia ei voitu antaa");
+        }
     }
 }

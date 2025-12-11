5e9fc3c23b8876956d1e358d87c73c6271294a07
Moona-T <s2300927@edu.bc.fi>
2025-12-11 09:44:23 +0200

    Kissa artit
    Erilaisia kissa artteja

--- TempoTabbies/Assets/Scripts/Cards2/DiscoCatScript.cs
+++ TempoTabbies/Assets/Scripts/Cards2/DiscoCatScript.cs
@@ -1,16 +1,61 @@
 using UnityEngine;
+using UnityEngine.WSA;
+using static CardDataScript;
 
 public class DiscoCatScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    PlayerScript holder;
+    CardData data;
+    float kauttoAika;
+    float CoolDown;
+    EffectStatus CurrentStatus;
+    CardManagerScript CardManager;
+
     void Start()
     {
-        
+        /* this.CardManager = GameObject.FindFirstObjectByType<CardManagerScript>();
+         this.data = CardManager.GetEffectDataforCard(EffectType.DiscoCat);*/
+        CurrentStatus = EffectStatus.cooldown;
+    }
+    
+    public void Activate(PlayerScript holder)
+    {
+        this.holder = holder;
+        this.CardManager = GameObject.FindFirstObjectByType<CardManagerScript>();
+        this.data = CardManager.GetEffectDataforCard(EffectType.DiscoCat);
+        CurrentStatus = EffectStatus.waiting; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (CurrentStatus)
+        {
+            case EffectStatus.waiting:
+
+                DiscoCatPanel.instance.StartTimer();
+                DiscoCatPanel.instance.canvas.gameObject.SetActive(true);
+                CurrentStatus = EffectStatus.active;
+                
+                break;
+
+            case EffectStatus.active:
+
+                //DiscoCatPanel.instance.panelImage.gameObject.SetActive(true);
+                //DiscoCatPanel.instance.canvas.gameObject.SetActive(true);
+                break;
+
+            case EffectStatus.cooldown:
+                    CoolDown += Time.deltaTime;
+                if (CoolDown < data.cooldown)
+                {
+                    CurrentStatus = EffectStatus.waiting;
+                }
+                else
+                {}
+                
+                break;
+        }
+
     }
 }
